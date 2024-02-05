cac6e95f21093d9137ee43bab7a24aa6e69bcbd2
“MatheusH” <matheus-cd@hotmail.com>
2024-02-05 17:14:24 -0300

    Organizando melhor o replace

--- script.js
+++ script.js
@@ -27,15 +27,25 @@ function copyText() {
 function encrypt() {
    hideDecryptInfo();
    getText(textToEncrypt);
-   encryptedText = text.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+   encryptedText = text
+      .replace(/e/g, "enter")
+      .replace(/i/g, "imes")
+      .replace(/a/g, "ai")
+      .replace(/o/g, "ober")
+      .replace(/u/g, "ufat");
+   
    insertText(textEncryptedArea, encryptedText);
 }
 
 function decrypt() {
    hideDecryptInfo();
    getText(textToEncrypt);
-   encryptedText = text.replace(/enter/g, "e").replace(/imer/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+   encryptedText = text
+      .replace(/enter/g, "e")
+      .replace(/imer/g, "i")
+      .replace(/ai/g, "a")
+      .replace(/ober/g, "o")
+      .replace(/ufat/g, "u");
+   
    insertText(textEncryptedArea, encryptedText);
 }
-
-// text.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
